283dba11c95d9d9d380f6cedca71fabbde26d3cf
maria <144173559+mabela2807@users.noreply.github.com>
2025-03-07 14:25:16 -0300

    teste

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded',function(){
-const aumentarFonteBotao = document.getElementById('aumentar-fonte');
-
-Let tamanhoAtualFonte =1;
-aumentarFonteBotao.addEventListener('click',function(){
-tamanhoAtualFonte += 0.1;
-document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-});
-
-});
+    const aumentarFonteBotao = document.getElementById('aumentar-fonte');
+    
+    Let tamanhoAtualFonte =1;
+    aumentarFonteBotao.addEventListener('click',function(){
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+    
+    });
