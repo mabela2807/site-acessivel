02fa25380ea80ee9846c61caea6a752922ad0bfd
maria <144173559+mabela2807@users.noreply.github.com>
2025-03-07 14:31:15 -0300

    fonte

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded',function(){
-    const aumentarFonteBotao = document.getElementById('aumentar-fonte');
-    
-    Let tamanhoAtualFonte =1;
-    aumentarFonteBotao.addEventListener('click',function(){
-    tamanhoAtualFonte += 0.1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+
+    let tamanhoAtualFonte = 1;
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
     });
-    
-    });
+});
