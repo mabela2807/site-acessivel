77b9e8b5e00e7375079687997b0fec0cd4d5e465
maria <144173559+mabela2807@users.noreply.github.com>
2025-03-07 14:14:04 -0300

    aumentar fonte

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded',function(){
 const aumentarFonteBotao = document.getElementById('aumentar-fonte');
+
+Let tamanhoAtualFonte =1;
 aumentarFonteBotao.addEventListener('click',function(){
 tamanhoAtualFonte += 0.1;
-document.body.style.fontSize = ${tamanhoAtualFonte}rem
-})
+document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+});
 
-})
+});
